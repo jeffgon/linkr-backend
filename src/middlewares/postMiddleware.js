7d835673c2b3ee34f2645dd84b5ae6ff44b031c2
Nishiyama13 <nishiyamaeel@gmail.com>
2023-03-10 13:29:28 -0300

    register new user

--- src/middlewares/postMiddleware.js
+++ src/middlewares/postMiddleware.js
@@ -1,6 +1,5 @@
-import db from "../database/db.js";
+import { db } from "../database/db.js";
 
-export async function postValidation(req, res, next){
-    
-    const token = req.headers.authorization?.replace("Bearer ", "");
+export async function postValidation(req, res, next) {
+  const token = req.headers.authorization?.replace("Bearer ", "");
 }
